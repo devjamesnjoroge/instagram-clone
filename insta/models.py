009a646128717285ef01d2aa1e2467fa4c727d4a
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-06-04 15:11:55 +0300

    ft: add post model

--- insta/models.py
+++ insta/models.py
@@ -8,3 +8,14 @@ class Profile(models.Model):
     profile_photo = CloudinaryField('image')
     bio = models.TextField(max_length=500)
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+class Post(models.Model):
+    image = CloudinaryField('image')
+    caption = models.TextField(max_length=500)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='likes')
+    comments = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='comments')
+
+
+
+
