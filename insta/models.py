1b61094a62ba3b859ae8258a625e8f2c458cd533
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-06-04 15:30:15 +0300

    ft: add methods to manipulate profile

--- insta/models.py
+++ insta/models.py
@@ -9,12 +9,31 @@ class Profile(models.Model):
     bio = models.TextField(max_length=500)
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
+    def create_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def get_profile(cls):
+        profile = Profile.objects.all()
+        return profile
+
+    @classmethod
+    def get_profile_by_id(cls, id):
+        profile = Profile.objects.get(id=id)
+        return profile
+
+    def __str__(self):
+        return self.user.username
+
 class Post(models.Model):
     image = CloudinaryField('image')
     caption = models.TextField(max_length=500)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='likes')
-    comments = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='comments')
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='likes', blank=True)
+    comments = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='comments', blank=True)
 
 class Comment(models.Model):
     comment = models.TextField(max_length=500)
@@ -28,4 +47,3 @@ class Like(models.Model):
 class Follow(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    
